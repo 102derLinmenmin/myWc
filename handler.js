b24947247ef24b506c66ccc997b7a944310e0537
yyl102zz <1627332137@qq.com>
2018-09-14 12:09:38 +0800

    [feat] 添加注释

--- handler.js
+++ handler.js
@@ -1,54 +1,66 @@
-// handler.js
+// handler.js 基本操作指令
 
 const fs = require("fs")
 const readline = require('readline')
 const path = require('path')
 
-const readLineHandle = (args) => {
-  let filepath = path.join(__dirname, args)
+/*
+ * 逐行读取文件，待下一步操作
+ * input: fileName：文件相对路径
+ * return：rl：逐行读取的文件内容
+ */
+const readLineHandle = (fileName) => {
+  let filepath = path.join(__dirname, fileName)
   let input = fs.createReadStream(filepath)
   return readline.createInterface({
     input: input
   })
 }
 
-const returnLinesNum = (args) => {
-  const rl = readLineHandle(args)
+// -l 指令 
+const returnLinesNum = (fileName) => {
+  const rl = readLineHandle(fileName)
   let lines = 0
+  // 逐行加一
   rl.on('line', (line) => {
     lines += 1
   })
   rl.on('close', () => {
-    console.log(`${args}文件的行数为: ${lines}`)
+    console.log(`${fileName}文件的行数为: ${lines}`)
   })
 }
 
-const returnWordsNum = (args) => {
-  const rl = readLineHandle(args)
+// -s 指令
+const returnWordsNum = (fileName) => {
+  const rl = readLineHandle(fileName)
   let words = []
+  // 对逐行的内容操作，以空格为分界计算单词数，压入单词栈
   rl.on('line', (line) => {
     const currentLineArr = line.trim().split(' ')
     const currentLine = currentLineArr.length === 0 ? line : currentLineArr
     words = [...words, ...currentLine]
   })
   rl.on('close', () => {
-    console.log(`${args}文件的单词数为: ${words.length}`)
+    console.log(`${fileName}文件的单词数为: ${words.length}`)
   })
 }
 
-const returnLettersNum = (args) => {
-  const rl = readLineHandle(args)
+// -c 指令
+const returnLettersNum = (fileName) => {
+  const rl = readLineHandle(fileName)
   let words = []
+  // 对逐行的内容操作，以空格为分界计算单词数，压入单词栈
   rl.on('line', (line) => {
     const currentLineArr = line.trim().split(' ')
     const currentLine = currentLineArr.length === 0 ? line : currentLineArr
     words = [...words, ...currentLine]
   })
   rl.on('close', () => {
+    // 逐行读取结束时，对单词栈的逐个单词长度累加，得字符数
     const wordsNum = words.reduce((acc, val) => {
       return acc + val.length
     }, 0)
-    console.log(`${args}文件的字母数为: ${wordsNum}`)
+    console.log(`${fileName}文件的字母数为: ${wordsNum}`)
   })
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 #!/usr/bin/env node
+const handlers = require('./constant')
+const recurseFolder = require('./recurseHandler')
 
-const { returnLinesNum, returnWordsNum, returnLettersNum } = require('./handler')
+const commandLine = process.argv  // 获取命令行输入
+const lineLength = commandLine.length
 
-const [ node, indexFile, command, args ]  = process.argv
+let commands = []
+let files = []
+let breakError = true
+let lastCommandIdx = 2
 
-const data = {
-  '-c': returnLettersNum,  //返回文件 file.c 的字符数
-  '-w': returnLinesNum,    //返回文件 file.c 的词的数目  
-  '-l': returnWordsNum    //返回文件 file.c 的行数
+handlers['-s'] = recurseFolder
+
+/* 对命令行输入进行操作
+ * 将指令压入指令集 commands
+ * 将可能存在的通配符匹配的文件压入所匹配的文件栈 files
+ */
+const readCommandLines = (async () => {
+	try {
+		commandLine.forEach((val, i) => {
+			if (i > 1 && i < lineLength - 1) {
+				const validCommand = val.indexOf('-') === 0
+				if (validCommand) {
+					// 验证指令是否存在在 handlers 中，不存在则跳出整个操作
+					if (handlers[val]) {
+						commands.push(val)
+						lastCommandIdx = i
+					} else {
+						console.log(`找不到相关指令: ${val}`)
+						throw new Error('cannot find such command')
+					}
+				}
+			}
+			breakError = false
+		})
+	} catch(e) {
+		breakError = true
+		console.log(e.message)
+	}
+})()
+
+/* 指令未有输入错误时可进行指令操作
+ * 执行指令与 data 所匹配的命令 
+ * 如果是对文件夹的操作则进行文件夹递归操作 recurseFolder
+ */
+if (!breakError) {
+	commandLine.splice(0, lastCommandIdx + 1)
+	files = commandLine
+	if (commands.indexOf('-s') > -1) {
+		commands = commands.filter((val) => {
+			return val !== '-s'
+		})
+		files.forEach((fileName) => {
+			recurseFolder(fileName, commands)
+		})
+	} else {
+		commands.forEach((command) => {
+			files.forEach((fileName) => {
+				// 执行指令与 data 所匹配的命令
+				handlers[command].call(this, fileName, command) 
+			})
+		})
+	}
 }
 
-data[command].call(this, args)
